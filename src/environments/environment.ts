a56cd04a4b6c8820e259b463f958343d18653f51
KaiqueSP96 <kaiqueba6@gmail.com>
2023-03-28 11:23:40 -0300

    add-navbar-component

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-1ea5e',
+    appId: '1:295978382898:web:8b0a10338d23d9957e2e2b',
+    storageBucket: 'password-manager-1ea5e.appspot.com',
+    apiKey: 'AIzaSyBcYuFXsWT8C8Wew70B2mpWlgmOGXmsrKQ',
+    authDomain: 'password-manager-1ea5e.firebaseapp.com',
+    messagingSenderId: '295978382898',
+  },
+    production: false
+}

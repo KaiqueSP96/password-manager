bd784cb4a1971fbfc5e94fa135cc94386f356f34
KaiqueSP96 <kaiqueba6@gmail.com>
2023-03-30 11:32:34 -0300

    add ngForm to form / more functionalities

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PasswordManagerService} from "../password-manager.service";
 
 @Component({
   selector: 'app-site-list',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./site-list.component.css']
 })
 export class SiteListComponent {
+
+  constructor(private passwordmanager: PasswordManagerService) {
+  }
+  onSubmit(values: object) {
+    console.log(values)
+    this.passwordmanager.addSite(values)
+      .then(()=>{
+        console.log("Data save Successfully")
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+
+  }
 
 }
